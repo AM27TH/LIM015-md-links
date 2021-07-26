be0d0c6ae33f89108d231ec8efae4d0c4f15796b
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-07-26 12:10:26 -0500

    Add options validate

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,40 @@
 #!/usr/bin/env node
 const process = require('process');
+const color = require('chalk');
+const API = require('./mdLinks.js');
 
-//Leer linea de comandos
 const argv = process.argv;
 
 function mdLinks(argv){
   const path = argv[2];
-  const options = argv.slice(3);
-  console.log(path, options);
+  let options = {};
+  switch(argv.slice(3).join(',')){
+    case '--stats,--validate':
+      options = { validate: true, stats: true};
+      break;
+    case '--validate':
+      options = { validate: true, stats: false};
+      break;
+    case '--stats':
+      options = { validate: false, stats: true};
+      break;
+    default:
+      options = {validate: false, stats: false};
+  }
+  API.mdLinks(path, options)
+  .then(links => {
+    if (options.stats) {
+      //stats.
+      //if validate
+    }else{
+      links.forEach((link) => {
+        console.log(color.magenta(link.file), color.cyan(link.href),
+          (options.validate)? ((link.ok === 'ok')? color.green(link.ok, link.status):color.red(link.ok, link.status)): '',
+          link.text);
+      });
+    }
+  })
+  .catch(error => console.log(color.bgRed(error)));
 }
 
 mdLinks(argv);
